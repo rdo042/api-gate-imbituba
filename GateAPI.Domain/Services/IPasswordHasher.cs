762c4437c988b7f46fb32494ed1670d2f1a91cd8
Yve Jose Zulatto <zulatto.yve@gmail.com>
2026-01-16 12:27:22 -0300

    feat: servico de hasher senha

---
+++ GateAPI.Domain/Services/IPasswordHasher.cs
@@ -0,0 +1,8 @@
+﻿namespace GateAPI.Domain.Services
+{
+    public interface IPasswordHasher
+    {
+        public string HashPassword(string password);
+        public bool VerifyPassword(string password, string hashedPassword);
+    }
+}
---
+++ GateAPI.Infra/Services/PasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using GateAPI.Domain.Services;
+
+namespace GateAPI.Infra.Services
+{
+    public class PasswordHasher : IPasswordHasher
+    {
+        public string HashPassword(string password) =>
+        BCrypt.Net.BCrypt.HashPassword(password);
+
+        public bool VerifyPassword(string password, string hashedPassword) =>
+            BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+    }
+}
