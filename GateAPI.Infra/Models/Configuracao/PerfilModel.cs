e816d698e69e3dcda84990b4c671b3a01bab5018
Yve Jose Zulatto <zulatto.yve@gmail.com>
2026-01-16 12:27:37 -0300

    model usuario

---
+++ GateAPI.Infra/Models/Configuracao/PerfilModel.cs
@@ -0,0 +1,11 @@
+﻿using GateAPI.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace GateAPI.Infra.Models.Configuracao
+{
+    public class PerfilModel : BaseModel
+    {
+        [StringLength(50)] public required string Nome { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
---
+++ GateAPI.Infra/Models/Configuracao/UsuarioModel.cs
@@ -0,0 +1,15 @@
+﻿using GateAPI.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace GateAPI.Infra.Models.Configuracao
+{
+    public class UsuarioModel : BaseModel
+    {
+        [StringLength(50)] public required string Nome { get; set; }
+        [StringLength(50)] public required string Email { get; set; }
+        [StringLength(50)] public required string SenhaHash { get; set; }
+        public Guid PerfilId { get; set; }
+        public PerfilModel Perfil { get; set; } = default!;
+        public StatusEnum Status { get; set; }
+    }
+}
