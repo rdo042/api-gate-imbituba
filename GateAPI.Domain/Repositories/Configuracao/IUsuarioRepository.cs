36926a6c6c9f6be2fa928f03c1c75803ee4a6f5b
Yve Jose Zulatto <zulatto.yve@gmail.com>
2026-01-16 12:27:53 -0300

    add: mapper e contrato de usuario

---
+++ GateAPI.Domain/Repositories/Configuracao/IUsuarioRepository.cs
@@ -0,0 +1,14 @@
+﻿using GateAPI.Domain.Entities.Configuracao;
+
+namespace GateAPI.Domain.Repositories.Configuracao
+{
+    public interface IUsuarioRepository
+    {
+        Task<Usuario?> GetByIdAsync(Guid id);
+        Task<Usuario?> GetByEmailAsync(string email);
+        Task<IEnumerable<Usuario>> GetAllAsync();
+        Task<Usuario> AddAsync(Usuario entidade);
+        Task UpdateAsync(Usuario entidade);
+        Task DeleteAsync(Guid id);
+    }
+}
---
+++ GateAPI.Infra/Mappers/Configuracao/UsuarioMapper.cs
@@ -0,0 +1,34 @@
+﻿using GateAPI.Domain.Entities.Configuracao;
+using GateAPI.Infra.Models.Configuracao;
+
+namespace GateAPI.Infra.Mappers.Configuracao
+{
+    public static class UsuarioMapper
+    {
+        public static Usuario ToDomain(UsuarioModel model, Perfil perfil)
+        {
+            //var perfil = Perfil.Load(model.Perfil.Id, model.Perfil.Nome, model.Status);
+
+            return Usuario.Load(
+                model.Id,
+                model.Nome,
+                model.Email,
+                model.SenhaHash,
+                perfil,
+                model.Status
+            );
+        }
+        public static UsuarioModel ToModel(Usuario entity)
+        {
+            return new UsuarioModel()
+            {
+                Id = entity.Id,
+                Nome = entity.Nome,
+                Email = entity.Email,
+                SenhaHash = entity.SenhaHash,
+                PerfilId = entity.Perfil.Id,
+                Status = entity.Status
+            };
+        }
+    }
+}
