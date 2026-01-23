b72d92220bd201b008a6db5ee4a8bb603e996a71
Zulatto <zulatto.yve@gmail.com>
2026-01-23 09:29:19 -0300

    ajuste reconhecer placa externa

--- GateAPI.Infra/Providers/ExternalLprProvider.cs
+++ GateAPI.Infra/Providers/ExternalLprProvider.cs
@@ -1,41 +1,41 @@
-﻿using GateAPI.Application.Providers;
-using GateAPI.Domain.Entities.Integracao;
-using Microsoft.Extensions.Logging;
-using System.Net.Http.Json;
+﻿//using GateAPI.Application.Providers;
+//using GateAPI.Domain.Entities.Integracao;
+//using Microsoft.Extensions.Logging;
+//using System.Net.Http.Json;
 
-namespace GateAPI.Infra.Providers
-{
-    public class ExternalLprProvider(
-        HttpClient http,
-        ILogger<ExternalLprProvider> logger) : ILprProvider
-    {
-        private readonly HttpClient _http = http;
-        private readonly ILogger<ExternalLprProvider> _logger = logger;
+//namespace GateAPI.Infra.Providers
+//{
+//    public class ExternalLprProvider(
+//        HttpClient http,
+//        ILogger<ExternalLprProvider> logger) : ILprProvider
+//    {
+//        private readonly HttpClient _http = http;
+//        private readonly ILogger<ExternalLprProvider> _logger = logger;
 
-        public async Task<Lpr?> RecognizeAsync(
-            string base64,
-            CancellationToken ct)
-        {
-            try
-            {
-                var response = await _http.PostAsJsonAsync(
-                    "api/Ocr/ProcessVehiclePlate",
-                    new { image = base64 },
-                    ct);
+//        public async Task<Lpr?> RecognizeAsync(
+//            string base64,
+//            CancellationToken ct)
+//        {
+//            try
+//            {
+//                var response = await _http.PostAsJsonAsync(
+//                    "api/Ocr/ProcessVehiclePlate",
+//                    new { image = base64 },
+//                    ct);
 
-                if (!response.IsSuccessStatusCode)
-                    return null;
+//                if (!response.IsSuccessStatusCode)
+//                    return null;
 
-                var result = await response.Content
-                    .ReadFromJsonAsync<Lpr>(ct);
+//                var result = await response.Content
+//                    .ReadFromJsonAsync<Lpr>(ct);
 
-                return result;
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Erro no LPR externo");
-                return null;
-            }
-        }
-    }
-}
+//                return result;
+//            }
+//            catch (Exception ex)
+//            {
+//                _logger.LogError(ex, "Erro no LPR externo");
+//                return null;
+//            }
+//        }
+//    }
+//}
