cec4afe7b0c4eb2c71bbaf5d18f4567da8417770
Zulatto <zulatto.yve@gmail.com>
2026-01-22 22:36:05 -0300

    feat: implementacao reconhecimento de placa ao enviar imagem em base64

---
+++ GateAPI/Controllers/Integracoes/AdaptersController.cs
@@ -0,0 +1,25 @@
+﻿using GateAPI.Application.UseCases.Integracao.ReconhecerPlacaUC;
+using GateAPI.Requests.Integracao;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GateAPI.Controllers.Integracoes
+{
+    [Route("api/adapters")]
+    [ApiController]
+    public class AdaptersController(
+        ILogger<AdaptersController> logger,
+        IMediator mediator
+        ) : BaseController(logger)
+    {
+        [HttpPost("/lpr/ler-placa")]
+        public async Task<IActionResult> Criar([FromBody] LprRequest data)
+        {
+            var command = new ReconhecerPlacaCommand(data.ImagemPlaca);
+
+            var result = await mediator.Send(command);
+
+            return result.IsSuccess ? OkResponse(result.Data) : BadRequestResponse(result.Error ?? "Erro ao reconhecer placa");
+        }
+    }
+}
