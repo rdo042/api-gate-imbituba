3e4e906d317d9a11e51c3c56b0cc81b3d732e24f
Yve Jose Zulatto <zulatto.yve@gmail.com>
2026-01-23 18:44:14 -0300

    feat: entidade tasks

---
+++ GateAPI.Domain/Entities/Configuracao/Task.cs
@@ -0,0 +1,29 @@
+﻿using GateAPI.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GateAPI.Domain.Entities.Configuracao
+{
+    public class Tasks : BaseEntity
+    {
+        public string Nome { get; private set; } = string.Empty;
+        public string Url { get; private set; } = string.Empty;
+        public StatusEnum Status { get; private set; }
+
+        private Tasks() { }
+
+        public static Tasks Load(Guid id, string nome, string url, StatusEnum status)
+        {
+            var entidade = new Tasks
+            {
+                Nome = nome,
+                Url = url,
+                Status = status
+            };
+
+            entidade.SetId(id);
+            return entidade;
+        }
+    }
+}
---
+++ GateAPI.Infra/Models/Configuracao/TasksModel.cs
@@ -0,0 +1,11 @@
+﻿using GateAPI.Domain.Enums;
+
+namespace GateAPI.Infra.Models.Configuracao
+{
+    public class TasksModel : BaseModel
+    {
+        public string Nome { get; set; }
+        public string Url { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
