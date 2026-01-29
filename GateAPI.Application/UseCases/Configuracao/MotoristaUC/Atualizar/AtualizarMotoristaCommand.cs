7fa7251924ac57ae203d220d71fde76cb09c258f
Carlos Abreu Jr <pis1114@supero.com.br>
2026-01-28 22:41:21 -0300

    feat: remove dataannotation

--- GateAPI.Application/UseCases/Configuracao/MotoristaUC/Atualizar/AtualizarMotoristaCommand.cs
+++ GateAPI.Application/UseCases/Configuracao/MotoristaUC/Atualizar/AtualizarMotoristaCommand.cs
@@ -1,19 +1,13 @@
 ﻿using GateAPI.Application.Common.Models;
-using GateAPI.Domain.Entities.Configuracao;
 using GateAPI.Domain.Enums;
 using MediatR;
-using System.ComponentModel.DataAnnotations;
 
 namespace GateAPI.Application.UseCases.Configuracao.MotoristaUC.Atualizar
 {
     public record AtualizarMotoristaCommand(Guid Id,
-    [Required(ErrorMessage = "O nome é obrigatório")]
-    [MinLength(3, ErrorMessage = "O nome deve ter no mínimo 3 caracteres")]
-    [MaxLength(100, ErrorMessage = "O nome deve ter no máximo 100 caracteres")]
     string Nome,
     DateOnly? DataNascimento,
     string RG,
-    [MinLength(11, ErrorMessage = "CPF deve ter no mínimo 11 caracteres")]
     string CPF,
     string CNH,
     DateOnly? ValidadeCnh,
